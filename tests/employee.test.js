a7514ba72bbe922a38a1899509b6afaa0f5eed7e
Hayley McHugh <hayleym522@gmail.com>
2021-11-22 13:22:56 -0800

    added some tests.js

---
+++ tests/employee.test.js
@@ -0,0 +1,33 @@
+const Employee = require('./lib/employee.js')
+
+test('generates an employee object', () => {
+    const employee = new Employee('Jane Doe', '1', 'janedoe@jane.com');
+
+    expect(employee.name).toBe('Jane Doe');
+    expect(employee.id).toBe('1');
+    expect(employee.email).toBe('janedoe@jane.com');
+});
+
+test('gets employees name', () => {
+    const employee = new Employee('Jane Doe', '1', 'janedoe@jane.com');
+
+    expect(employee.getName()).toEqual(expect.stringContaining('Jane Doe'));
+});
+
+test('gets employees id', () => {
+    const employee = new Employee('Jane Doe', '1', 'janedoe@jane.com');
+
+    expect(employee.getID()).toEqual(expect.stringContaining('1'));
+});
+
+test('gets employees email', () => {
+    const employee = new Employee('Jane Doe', '1', 'janedoe@jane.com');
+
+    expect(employee.getEmail()).toEqual(expect.stringContaining('janedoe@jane.com'));
+});
+
+test('gets employees role', () => {
+    const employee = new Employee('Jane Doe', '1', 'janedoe@jane.com');
+
+    expect(employee.getRole()).toEqual(expect.stringContaining('Employee'));
+});
---
+++ tests/engineer.test.js
@@ -0,0 +1,22 @@
+const Engineer = require('./lib/engineer.js')
+
+test('generates an engineer object', () => {
+    const engineer = new Engineer ('Sam Smith', '2', 'samsmith@smith.com', 'samsmithit')
+
+    expect(engineer.name).toBe('Sam Smith');
+    expect(engineer.id).toBe('2');
+    expect(engineer.email).toBe('samsmith@smith.com');
+    expect(engineer.github).toBe('samsmithgit');
+});
+
+test('gets engineers github username', () => {
+    const engineer = new Engineer ('Sam Smith', '2', 'samsmith@smith.com', 'samsmithit')
+
+    expect(engineer.getGitHub()).toEqual(expect.stringContaining('samsmithgit'));
+});
+
+test('gets engineers role', () => {
+    const engineer = new Engineer ('Sam Smith', '2', 'samsmith@smith.com', 'samsmithit')
+    
+    expect(engineer.getRole()).toEqual(expect.stringContaining('Engineer'))
+});
